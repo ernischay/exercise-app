375c77a328d446630982fc4a9c94513cbbcb0fd0
ernischay <er.nischay@gmail.com>
2022-03-28 12:22:01 +0530

    added local storage

--- src/stores/ExerciseStore.ts
+++ src/stores/ExerciseStore.ts
@@ -6,13 +6,19 @@ export class ExerciseStore {
 
     constructor() {
         makeAutoObservable(this)
+        const list = localStorage.getItem('exercise')
+        if (typeof list === 'string') {
+            this.exerciseList = JSON.parse(list)
+        }
     }
 
     addExercise(exercise: IExercise) {
         this.exerciseList.push(exercise)
+        localStorage.setItem('exercise', JSON.stringify(this.exerciseList))
     }
 
     clearExercise() {
         this.exerciseList = []
+        localStorage.clear()
     }
 }
